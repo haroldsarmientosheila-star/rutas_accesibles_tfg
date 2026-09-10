de4b415211d91465bbb87bed7561a95f6932d27d
Sheila HS <haroldsarmientosheila@gmail.com>
2026-09-11 00:39:55 +0100

    eliminar boton instalar porque no funciona

--- static/js/mapa.js
+++ static/js/mapa.js
@@ -141,23 +141,3 @@ if ("serviceWorker" in navigator) {
         });
 
 }
-
-let eventoInstalacion;
-
-window.addEventListener("beforeinstallprompt", (e) => {
-    e.preventDefault();
-    eventoInstalacion = e;
-
-    document.getElementById("instalar-app").style.display = "block";
-});
-
-document.getElementById("instalar-app").addEventListener("click", async () => {
-    if (!eventoInstalacion) return;
-
-    eventoInstalacion.prompt();
-
-    await eventoInstalacion.userChoice;
-
-    eventoInstalacion = null;
-    document.getElementById("instalar-app").style.display = "none";
-});
